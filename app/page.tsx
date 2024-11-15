c449b7b00321dbe67d286d71130e140940781976
Salma <elfalahsalma3@gmail.com>
2024-11-15 01:49:38 -0500

    completed chapter 9

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ import { lusitana } from '@/app/ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className= {styles.shape}>
-        {<AcmeLogo /> }
+      <div className={styles.shape}>
+        {<AcmeLogo />}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -31,12 +31,12 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
